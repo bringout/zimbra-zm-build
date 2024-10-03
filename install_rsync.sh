fb2752cf3fcc30d1927f7f5f642bbb584e43197a
Ernad Husremovic <hernad@bring.out.ba>
2024-10-03 06:48:28 +0200

    repos

--- install_rsync.sh
+++ install_rsync.sh
@@ -2,7 +2,7 @@
 
 FILE=/etc/yum.repos.d/CentOS-AppStream.repo
 cat > $FILE <<- EOM
-[appstream]
+[AppStream]
 name=CentOS-$releasever - AppStream
 #mirrorlist=http://#mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=AppStream&infra=$infra
 baseurl=http://vault.centos.org/$contentdir/$releasever/AppStream/$basearch/os/
@@ -13,7 +13,7 @@ EOM
 
 FILE=/etc/yum.repos.d/CentOS-Base.repo
 cat > $FILE <<- EOM
-[baseos]
+[BaseOS]
 name=CentOS-$releasever - Base
 #mirrorlist=http://#mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=BaseOS&infra=$infra
 baseurl=http://vault.centos.org/$contentdir/$releasever/BaseOS/$basearch/os/
@@ -24,7 +24,7 @@ EOM
 
 FILE=/etc/yum.repos.d/CentOS-Extras.repo
 cat > $FILE <<- EOM
-[extras]
+[Extras]
 name=CentOS-$releasever - Extras
 #mirrorlist=http://#mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra
 baseurl=http://vault.centos.org/$contentdir/$releasever/extras/$basearch/os/
