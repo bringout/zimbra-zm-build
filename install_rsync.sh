b6c364275674136326c52f4e62d5e5968bf39cae
Ernad Husremovic <hernad@bring.out.ba>
2024-10-03 06:58:52 +0200

    install_rsync.sh

--- install_rsync.sh
+++ install_rsync.sh
@@ -4,8 +4,8 @@ FILE=/etc/yum.repos.d/CentOS-AppStream.repo
 cat > $FILE <<- EOM
 [AppStream]
 name=CentOS-$releasever - AppStream
-#mirrorlist=http://#mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=AppStream&infra=$infra
-baseurl=http://vault.centos.org/$contentdir/$releasever/AppStream/$basearch/os/
+#mirrorlist=http://#mirrorlist.centos.org/?release=\$releasever&arch=\$basearch&repo=AppStream&infra=\$infra
+baseurl=http://vault.centos.org/\$contentdir/\$releasever/AppStream/\$basearch/os/
 gpgcheck=1
 enabled=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
@@ -15,8 +15,8 @@ FILE=/etc/yum.repos.d/CentOS-Base.repo
 cat > $FILE <<- EOM
 [BaseOS]
 name=CentOS-$releasever - Base
-#mirrorlist=http://#mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=BaseOS&infra=$infra
-baseurl=http://vault.centos.org/$contentdir/$releasever/BaseOS/$basearch/os/
+#mirrorlist=http://#mirrorlist.centos.org/?release=\$releasever&arch=\$basearch&repo=BaseOS&infra=\$infra
+baseurl=http://vault.centos.org/\$contentdir/\$releasever/BaseOS/\$basearch/os/
 gpgcheck=1
 enabled=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
@@ -26,13 +26,14 @@ FILE=/etc/yum.repos.d/CentOS-Extras.repo
 cat > $FILE <<- EOM
 [Extras]
 name=CentOS-$releasever - Extras
-#mirrorlist=http://#mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra
-baseurl=http://vault.centos.org/$contentdir/$releasever/extras/$basearch/os/
+#mirrorlist=http://#mirrorlist.centos.org/?release=\$releasever&arch=\$basearch&repo=extras&infra=\$infra
+baseurl=http://vault.centos.org/\$contentdir/\$releasever/extras/\$basearch/os/
 gpgcheck=1
 enabled=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 EOM
 
+yum update -y
 yum install -y rsync vim
 
 rsync --version
