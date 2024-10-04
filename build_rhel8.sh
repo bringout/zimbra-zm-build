6fb6366f063183c9b9d186f6edfd6e2c4df9907b
Ernad Husremovic <hernad@bring.out.ba>
2024-10-04 11:24:49 +0200

    rhel8

---
+++ build_rhel8.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo 10.1.x brach
+
+DEF_TAGS="10.1.2,10.1.1,10.1.0"
+
+BUILD_ARCH=x86_64
+RC=XBRING
+OS=RHEL9_x86_64
+Z_RELEASE=DAFFODIL
+RELEASE=10.1.2
+export RPM_BUILD_ROOT=/home/build/rpmbuild
+
+export PKG_OS_TAG
+
+./build.pl --ant-options -DskipTests=true \
+    --git-default-tag=$DEF_TAGS --build-type=FOSS \
+	--build-release=$Z_RELEASE \
+	--build-release-no=$RELEASE  \
+	--build-type=FOSS \
+	--build-release=$Z_RELEASE \
+	--build-release-candidate=$RC \
+	--build-thirdparty-server=files.zimbra.com \
+	--build-arch=$BUILD_ARCH \
+	--build-os=$OS \
+	--no-interactive
+
+
