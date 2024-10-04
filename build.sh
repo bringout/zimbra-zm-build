7c0bb46ad2e7a913836f2fb31ede79caf42decfa
Ernad Husremovic <hernad@bring.out.ba>
2024-10-04 10:47:29 +0200

    10.1.2

--- build.sh
+++ build.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-echo 10.0.x brach
+echo 10.1.x brach
 
-DEF_TAGS="10.0.9,10.0.8,10.0.7,10.0.6,10.0.5,10.0.4,10.0.3,10.0.2,10.0.1,10.0.0-GA"
+DEF_TAGS="10.1.2,10.1.1,10.1.0"
 
 BUILD_ARCH=x86_64
 RC=XBRING
 OS=RHEL9_x86_64
-Z_RELEASE=LIBERTY
-RELEASE=10.0.9
+Z_RELEASE=DAFFODIL
+RELEASE=10.1.2
 
 export PKG_OS_TAG
 
 ./build.pl --ant-options -DskipTests=true \
-    --git-default-tag=$DEF_TAGS --build-release-no=10.0.8 --build-type=FOSS --build-release=$Z_RELEASE \
+    --git-default-tag=$DEF_TAGS --build-type=FOSS \
+	--build-release=$Z_RELEASE \
 	--build-release-no=$RELEASE  \
 	--build-type=FOSS \
 	--build-release=$Z_RELEASE \
--- taguj.sh
+++ taguj.sh
@@ -1,5 +1,5 @@
 
-TAG=10.0.9
+TAG=10.1.2
 CUR=$(pwd)
 cd ..
 for dir in zm-* neko* ant* ical* java-html-saniti* antisamy ; do ls -ld $dir ; cd $dir ; git checkout $TAG || git tag $TAG ; cd .. ; done
